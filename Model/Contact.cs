53a8b20607251268cd1730f055c39e22852f12f7
Guest User <Guest@Epicodus-6E.local>
2018-02-09 12:05:27 -0800

    add Model Contact class and static List

--- Model/Contact.cs
+++ Model/Contact.cs
@@ -1,14 +1,71 @@
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
 using System;
-using Contacts.Models;
 
-namespace Contacts.Controllers
+namespace Contacts.Models
 {
-  public class HomeController : Controller
+  public class Contact
   {
-    [HttpGet("/")]
-        public ActionResult Index()
-        
+    private string _name;
+    private string _contactAddress;
+    private int _contactNumber;
+    private int _id;
+    private static List<Contact> _instances = new List<Contact> {};
+
+    public Contact (string name, string contactAddress, long contactNumber)
+    {
+      _name = name;
+      _contactAddress = contactAddress;
+      _contactNumber = contactNumber;
+      _id = _instances.Count;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name  = newName;
+    }
+    public string GetContactAddress()
+    {
+      return _contactAddress;
+    }
+    public void SetContactAddress(string newContactAddress)
+    {
+      _contactAddress = newContactAddress;
+    }
+    public long GetContactNumber()
+    {
+      return _contactNumber;
+    }
+    public void SetContactNumber(long newContactNumber)
+    {
+      _contactNumber = newContactNumber;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public void ContactSave()
+    {
+      _instances.Add(this);
+    }
+    public void ContactDelete()
+    {
+      _instances.Remove(this);
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+    public static List<Contact> DelAll()
+    {
+      _instances.Clear();
+      return _instances;
+    }
   }
 }
